f3b8faca68065696ad1e458cee3318dbee38c514
EmmaIreland <irela065@morris.umn.edu>
2013-02-12 19:52:58 -0600

    Working on simplifying boolean expressions. Wrote a few of the cases, still have to write a few more. Also need to write the negation-args function.

--- src/SAT_evaluation.clj
+++ src/SAT_evaluation.clj
@@ -1,12 +1,13 @@
 (ns SAT-evaluation)
 
 ;; Arguments are in a vector/list and are forms of strings [a-coll]
-;; each clause must be seperated by a space. example: ( p ^ q )
+;; Arguments must be valid arguments, any syntax error will return "error in argument"
 
 ;; and is represented as ^
 ;; or is represented as |
 ;; parentheses are represented as ()
 ;; not is represented as ~
+;; each clause must be seperated by a space. example: ( p ^ q )
 
 ;; solution variables are in a vector/list and the form of a map where keys = string variables and values = true or false [k-coll]
 ;; example: [{"p" true "f" false "t" true} {"p" false "f" true "t" true}]
@@ -15,21 +16,89 @@
 (defn semantics [a]
   (loop [arg a counter 0]
     (cond
+      ;; termination when we have only true or false left in list
       (and (= (count arg) 1) (first arg)) (first arg)
+      ;; the case where it's ~ boolean
+      (and
+        (= "~" (nth arg counter))
+        (or (= (nth arg  (inc counter)) true) (= (nth arg (inc counter)) false)))
+                           (if (= (nth arg (inc counter)) true) (recur (concat (take counter arg) (list false) (nthrest arg (+ counter 2)) ) counter)
+                                                                (recur (concat (take counter arg) (list true) (nthrest arg (+ counter 2)) ) counter)
+                             
+                             )
+      ;; the case where it's ~ ( some args )
+      (and
+        (= "~" (nth arg counter))
+        (= "(" (nth arg (inc counter)))) (recur (concat (take counter arg) (negation-args (nthrest arg (inc counter)))) 0)
+      ;; When we are at the front of the list
       (= (counter arg) 0) (recur arg (inc counter))
-      (= (counter arg) (dec (count arg))) (recur arg 0)
-      
+      ;; When we are at the end of the list
+      (= (counter arg) (dec (count arg))) (recur arg 0)      
       ;; the case where it's ( boolean )
       (and
         (= "(" (nth arg (dec counter))) 
         (= ")" (nth arg (inc counter))) 
-        (or (= (nth arg counter) true) (= (nth arg counter) false))) ()
+        (or (= (nth arg counter) true) (= (nth arg counter) false))) 
+                           (recur (concat (take (dec counter) arg) (list (nth arg counter)) (nthrest arg (+ counter 2)) ) (dec counter))
+      ;; the case where it's ( some args )
+      (= "(" (nth arg counter)) (recur (.indexOf (nthrest arg counter) ")") (semantics (take (nthrest arg (inc counter)) ) ) 0)
       
-      ;; need cases for:
-      ;; bool ^ bool
-      ;; bool | bool
-      ;; ~ bool
-      ;; ~ ( stuff )
+      ;; the case where it's true ^ true
+      (and
+        (= true (nth arg (dec counter)))
+        (= true (nth arg (inc counter)))
+        (= (nth arg counter) "^"))
+                           (recur (concat (take (dec counter) arg) (list true) (nthrest arg (+ counter 3)) ) (dec counter))
+        
+      ;; all other cases of boolean ^ boolean
+      (or
+        (and 
+          (= true (nth arg (dec counter)))
+          (= false (nth arg (inc counter)))
+          (= (nth arg counter) "^")
+         )
+        (and
+          (= false (nth arg (dec counter)))
+          (= true (nth arg (inc counter)))
+          (= (nth arg counter) "^")
+         )
+        (and
+          (= false (nth arg (dec counter)))
+          (= false (nth arg (inc counter)))
+          (= (nth arg counter) "^")
+         ))                (recur (concat (take (dec counter) arg) (list false) (nthrest arg (+ counter 3)) ) (dec counter))
+      
+      ;; the case where it's false | false
+      (and
+        (= false (nth arg (dec counter)))
+        (= false (nth arg (inc counter)))
+        (= (nth arg counter) "|"))
+                           (recur (concat (take (dec counter) arg) (list false) (nthrest arg (+ counter 3)) ) (dec counter))
+              
+      ;; all other cases of boolean | boolean
+      (or
+        (and 
+          (= true (nth arg (dec counter)))
+          (= true (nth arg (inc counter)))
+          (= (nth arg counter) "|")
+         )
+        (and
+          (= true (nth arg (dec counter)))
+          (= false (nth arg (inc counter)))
+          (= (nth arg counter) "|")
+         )
+        (and
+          (= false (nth arg (dec counter)))
+          (= true (nth arg (inc counter)))
+          (= (nth arg counter) "|")
+         ))                (recur (concat (take (dec counter) arg) (list true) (nthrest arg (+ counter 3)) ) (dec counter))
+      
+      
+      
+      ;;NEED TO MOVE ~ CASES UP THE COND LIST
+      ;;NEED CASE FOR (some arg)
+      ;;NEED TO WRITE negation-args
+
    )
   )
 )
@@ -47,6 +116,6 @@
   (pmap #(pmap (fn [x] (semantics x)) %) replace-all-vars))
 )
 
-;["p ^ ( q | r )" "( ~ p ^ q ) | r"] [{"p" true, "q" false, "r" true} {"p" true, "q" false, "r" false} {"p" false, "q" true, "r" true}]
+;["p ^ ( q | r )" "( ~ p ^ q ) | r"] [{"p" true "q" false "r" true} {"p" true "q" false "r" false} {"p" false "q" true "r" true}]
 
-;["( ~ p ^ q ) | r"] [{"p" true, "q" false, "r" true} {"p" true, "q" false, "r" false} {"p" false, "q" true, "r" true}]
+;["( ~ p ^ q ) | r"] [{"p" true "q" false "r" true} {"p" true "q" false "r" false} {"p" false "q" true "r" true}]
